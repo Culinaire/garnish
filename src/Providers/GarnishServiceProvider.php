1dbec60d33c73c1a03f92a7bab42564cee70c060
Frank McCoy <frankmccoy.d@gmail.com>
2018-03-25 08:51:12 -0500

    stubs

---
+++ src/Providers/GarnishServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Culinaire\Garnish;
+
+use Illuminate\Support\ServiceProvider;
+
+class GarnishServiceProvider extends ServiceProvider
+{
+  public function boot()
+  {
+    //
+  }
+
+  public function register()
+  {
+    //
+  }
+}
